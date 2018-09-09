3fa25ce660b9eefa98d70b6a05ea5d3990c3d0ae
Daichi Hioki <hiokidaichi@gmail.com>
2018-09-09 15:38:01 +0900

    Enable serial download using Range header

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -4,10 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+
+	"github.com/hioki-daichi/parallel-download/bytesranger"
 )
 
 var errExist = errors.New("downloader: file already exists")
@@ -19,11 +20,14 @@ type Downloader struct {
 
 	// URL represents the download URL.
 	URL string
+
+	// Parallelism represents how many parallel downloads.
+	Parallelism int
 }
 
 // NewDownloader returns new Downloader.
 func NewDownloader(w io.Writer, url string) *Downloader {
-	return &Downloader{OutStream: w, URL: url}
+	return &Downloader{OutStream: w, URL: url, Parallelism: 4} // TODO: Use flags instead of hard-coded 4
 }
 
 // Download downloads the file with the specified URL.
@@ -33,14 +37,19 @@ func (d *Downloader) Download() error {
 		return errExist
 	}
 
-	resp, err := http.Get(d.URL)
+	bytesrangeStrings, err := d.getBytesrangeStrings()
 	if err != nil {
 		return err
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
+	responses := make([]*http.Response, 0)
+	for _, bytesrangeString := range bytesrangeStrings {
+		resp, err := d.getHTTPResponseWithinRange(bytesrangeString)
+		if err != nil {
+			return err
+		}
+
+		responses = append(responses, resp)
 	}
 
 	fp, err := os.OpenFile(filename, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
@@ -48,14 +57,38 @@ func (d *Downloader) Download() error {
 		return err
 	}
 
-	_, err = fp.Write(body)
-	if err != nil {
-		return err
+	for _, resp := range responses {
+		_, err := io.Copy(fp, resp.Body)
+		if err != nil {
+			return err
+		}
 	}
 
 	fmt.Fprintf(d.OutStream, "Downloaded: %q\n", d.URL)
 
 	return nil
+}
+
+func (d *Downloader) getBytesrangeStrings() ([]string, error) {
+	resp, err := http.Head(d.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytesranger.Split(int(resp.ContentLength), d.Parallelism)
+}
+
+func (d *Downloader) getHTTPResponseWithinRange(rangeString string) (*http.Response, error) {
+	client := &http.Client{Timeout: 0}
+
+	req, err := http.NewRequest("GET", d.URL, nil)
+	if err != nil {
+		return &http.Response{}, err
+	}
+
+	req.Header.Set("Range", rangeString)
+
+	return client.Do(req)
 }
 
 func isFileExist(filename string) bool {
