b05954c6612ee6ac500b81bc3afbba4a91941955
Daichi Hioki <hiokidaichi@gmail.com>
2018-09-17 22:45:26 +0900

    Collect functions in main

--- main.go
+++ main.go
@@ -1,22 +1,139 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"path"
 
-	"github.com/hioki-daichi/parallel-download/downloader"
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	errExist = errors.New("file already exists")
 )
 
 func main() {
 	url := os.Args[1]
-	err := execute(os.Stdout, url)
+	err := newDownloader(os.Stdout, url).download()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func execute(w io.Writer, url string) error {
-	d := downloader.NewDownloader(w, url)
-	return d.Download()
+type downloader struct {
+	outStream   io.Writer
+	url         string
+	parallelism int
+}
+
+func newDownloader(w io.Writer, url string) *downloader {
+	return &downloader{outStream: w, url: url, parallelism: 8} // TODO: Use flags instead of hard-coded 8
+}
+
+func (d *downloader) download() error {
+	_, filename := path.Split(d.url)
+	_, err := os.Stat(filename)
+	if !os.IsNotExist(err) {
+		return errExist
+	}
+
+	resp, err := http.Head(d.url)
+	if err != nil {
+		return err
+	}
+
+	rangeStrings, err := toRangeStrings(int(resp.ContentLength), d.parallelism)
+	if err != nil {
+		return err
+	}
+
+	responses := map[int]*http.Response{}
+
+	eg := errgroup.Group{}
+	for i, rangeString := range rangeStrings {
+		i := i
+		rangeString := rangeString
+		eg.Go(func() error {
+			client := &http.Client{Timeout: 0}
+
+			req, err := http.NewRequest("GET", d.url, nil)
+			if err != nil {
+				return err
+			}
+
+			req.Header.Set("Range", rangeString)
+
+			resp, err := client.Do(req)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(d.outStream, "i: %d, ContentLength: %d, Range: %s\n", i, resp.ContentLength, rangeString)
+			responses[i] = resp
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+
+	fp, err := os.OpenFile(filename, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(responses); i++ {
+		resp := responses[i]
+		_, err := io.Copy(fp, resp.Body)
+		if err != nil {
+			os.Remove(filename)
+			return err
+		}
+	}
+
+	fmt.Fprintf(d.outStream, "Downloaded: %q\n", d.url)
+
+	return nil
+}
+
+func toRangeStrings(contentLength int, parallelism int) ([]string, error) {
+	rangeStructs := make([]rangeStruct, 0)
+
+	if parallelism == 0 {
+		parallelism = 1
+	}
+
+	if contentLength < parallelism {
+		parallelism = contentLength
+	}
+
+	length := contentLength / parallelism
+
+	i := 0
+	for n := parallelism; n > 0; n-- {
+		first := i
+		i += length
+		last := i - 1
+		rangeStructs = append(rangeStructs, rangeStruct{first: first, last: last})
+	}
+
+	if rem := contentLength % parallelism; rem != 0 {
+		rangeStructs[len(rangeStructs)-1].last += rem
+	}
+
+	rangeStrings := make([]string, 0)
+
+	for _, rangeStruct := range rangeStructs {
+		rangeStrings = append(rangeStrings, fmt.Sprintf("bytes=%d-%d", rangeStruct.first, rangeStruct.last))
+	}
+
+	return rangeStrings, nil
+}
+
+type rangeStruct struct {
+	first int
+	last  int
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMain_toRangeStrings(t *testing.T) {
+	cases := map[string]struct {
+		contentLength int
+		parallelism   int
+		expected      []string
+	}{
+		"contentLength: 5, parallelism: 0": {contentLength: 5, parallelism: 0, expected: []string{"bytes=0-4"}},
+		"contentLength: 5, parallelism: 1": {contentLength: 5, parallelism: 1, expected: []string{"bytes=0-4"}},
+		"contentLength: 5, parallelism: 2": {contentLength: 5, parallelism: 2, expected: []string{"bytes=0-1", "bytes=2-4"}},
+		"contentLength: 5, parallelism: 3": {contentLength: 5, parallelism: 3, expected: []string{"bytes=0-0", "bytes=1-1", "bytes=2-4"}},
+		"contentLength: 5, parallelism: 4": {contentLength: 5, parallelism: 4, expected: []string{"bytes=0-0", "bytes=1-1", "bytes=2-2", "bytes=3-4"}},
+		"contentLength: 5, parallelism: 5": {contentLength: 5, parallelism: 5, expected: []string{"bytes=0-0", "bytes=1-1", "bytes=2-2", "bytes=3-3", "bytes=4-4"}},
+		"contentLength: 5, parallelism: 6": {contentLength: 5, parallelism: 6, expected: []string{"bytes=0-0", "bytes=1-1", "bytes=2-2", "bytes=3-3", "bytes=4-4"}},
+	}
+
+	for n, c := range cases {
+		c := c
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			contentLength := c.contentLength
+			parallelism := c.parallelism
+			expected := c.expected
+
+			actual, err := toRangeStrings(contentLength, parallelism)
+			if err != nil {
+				t.Fatalf("err %s", err)
+			}
+			if !reflect.DeepEqual(actual, expected) {
+				t.Errorf(`expected="%s" actual="%s"`, expected, actual)
+			}
+		})
+	}
+}
