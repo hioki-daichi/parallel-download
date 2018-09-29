05a5d2c2ff93fc8b396f8f43d32d3b3d02789f75
Daichi Hioki <hiokidaichi@gmail.com>
2018-09-29 11:19:20 +0900

    cosme

--- main.go
+++ main.go
@@ -23,8 +23,8 @@ func execute(w io.Writer, args []string) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	interruptor.RegisterCleanFunction(cancel)
 	interruptor.Setup(w)
+	interruptor.RegisterCleanFunction(cancel)
 
 	opts, err := opt.Parse(args...)
 	if err != nil {
